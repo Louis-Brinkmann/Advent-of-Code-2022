46e2071dca89277defdb3de7d302c70a3b06d6a1
Louis Brinkmann <>
2022-12-04 15:20:58 +0100

    RockPaperScissors first half complete

--- src/main/01CalorieCounting.cs
+++ src/main/01CalorieCounting.cs
@@ -11,17 +11,17 @@ namespace Advent_of_Code_2022.src.main
 {
     internal class CalorieCounting
     {
-        static public void Main(string[] args)
-        {
-            var caloryList = ReadInput("D:\\repos\\Advent-of-Code-2022\\lib\\CalorieCounting.txt");
-            int[] mostCalories = FindThreeBiggestItemsInList(caloryList);
-            int total = 0;
-            foreach(int item in mostCalories )
-            {
-                total += item;
-            }
-            Console.WriteLine(total);
-        }
+        //static public void Main(string[] args)
+        //{
+        //    var caloryList = ReadInput("D:\\repos\\Advent-of-Code-2022\\lib\\CalorieCounting.txt");
+        //    int[] mostCalories = FindThreeBiggestItemsInList(caloryList);
+        //    int total = 0;
+        //    foreach(int item in mostCalories )
+        //    {
+        //        total += item;
+        //    }
+        //    Console.WriteLine(total);
+        //}
         
         static private List<int> ReadInput(string filePath)
         {
---
+++ src/main/02RockPaperScissors.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.PortableExecutable;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advent_of_Code_2022.src.main
+{
+    internal class RockPaperScissors
+    {
+        static public void Main(String[] args)
+        {
+            var games = ReadInput("D:\\repos\\Advent-of-Code-2022\\lib\\RockPaperScissors.txt");
+            var score = 0;
+
+            foreach(string game in games)
+            {
+                char opponentsTurn = Char.Parse(game.Split()[0]);
+                char myTurn = Char.Parse(game.Split()[1]);
+                score += CalculatePoints(opponentsTurn, myTurn);
+            }
+            Console.WriteLine(score);
+        }
+
+        static private List<string> ReadInput(string filePath)
+        {
+            List<string> games = new List<string>();
+
+            foreach (string line in System.IO.File.ReadLines(filePath))
+            {
+                games.Add(line);
+            }
+            return games;
+        }
+
+        /*
+         * A: Rock; B: Paper; C: Scissors
+         * X: Rock; Y: Paper; Z: Scissors
+         * Possible outcomes:
+         * AX: 3 + 1 = 4
+         * AY: 6 + 2 = 8
+         * AZ: 0 + 3 = 3
+         * BX: 0 + 1 = 1
+         * BY: 3 + 2 = 5
+         * BZ: 6 + 3 = 9
+         * CX: 6 + 1 = 7
+         * CY: 0 + 2 = 2
+         * CZ: 3 + 3 = 6
+         */
+
+        static private int CalculatePoints(char opponentsTurn, char myTurn)
+        {
+            int points = 0;
+
+            switch(opponentsTurn)
+            {
+                case 'A':
+                    points = CaseA(myTurn);
+                    break;
+
+                case 'B':
+                    points = CaseB(myTurn);
+                    break;
+
+                case 'C':
+                    points = CaseC(myTurn);
+                    break;
+            }
+            
+            return points;
+        }
+
+        private static int CaseA(char myTurn)
+        {
+            int points = 0;
+
+            switch (myTurn)
+            {
+                case 'X':
+                    points = 4;
+                    break;
+
+                case 'Y':
+                    points = 8;
+                    break;
+
+                case 'Z':
+                    points = 3;
+                    break;
+            }
+            return points;
+        }
+
+        private static int CaseB(char myTurn)
+        {
+            int points = 0;
+
+            switch (myTurn)
+            {
+                case 'X':
+                    points = 1;
+                    break;
+
+                case 'Y':
+                    points = 5;
+                    break;
+
+                case 'Z':
+                    points = 9;
+                    break;
+            }
+            return points;
+        }
+        
+        private static int CaseC(char myTurn)
+        {
+            int points = 0;
+
+            switch (myTurn)
+            {
+                case 'X':
+                    points = 7;
+                    break;
+
+                case 'Y':
+                    points = 2;
+                    break;
+
+                case 'Z':
+                    points = 6;
+                    break;
+            }
+            return points;
+        }
+    }
+}
